e287e9c5309c6c5fc04dbd9c3a5f805a0c9e0d03
Simon Olofsson <telsak@hotmail.se>
2026-03-19 13:31:00 +0100

    First flat design, routes in myproject/

---
+++ myproject/models.py
@@ -0,0 +1,8 @@
+from .extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+    def __repr__(self):
+        return f'<User "{self.name}">'
